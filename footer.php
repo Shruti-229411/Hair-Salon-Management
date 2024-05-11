2c9c64d1c7166e5c3d593ad61476fcf5ac5b127d
Shruti-229411 <126856208+Shruti-229411@users.noreply.github.com>
2024-05-11 12:28:43 +0530

    Add files via upload

---
+++ footer.php
@@ -0,0 +1,72 @@
+        
+			   </div>
+		   </div>
+    	     
+	      
+				
+			   </div>
+		  </div>
+      
+ 
+				
+	   <div class="clear"></div>
+     </div>
+    </div>
+  </div>
+  <div class="footer-strip"> </div>
+ <div class="footer">
+   	  <div class="wrap">
+   	    <div class="footer_grides">
+   	    	<div class="footer_grid1">
+					<h3>Information</h3>
+								<ul>
+						            <li><a href="#">About Us</a></li>
+						     		<li><a href="#">Privacy Policy</a></li>
+						     		<li><a href="#">Newsletter</a></li>
+						     		<li><a href="#">Site Map</a></li>						     		
+						   	   </ul>	
+						
+					  	</div>
+				<div class="footer_grid2">
+					<h3>Get In Touch</h3>
+							<div class="address">
+							<ul>
+						  	 <li>Milano Salon,</li>
+						  	  <li>Mumbai,</li>
+						  	   <li>Maharashtra.</li>
+						  	 <li>www.milano@gmail.com</li>
+						  	 <li><span>Telephone :</span> +91869877087</li>
+						  </ul>
+				   </div>				  
+			     </div>
+				<div class="footer_grid3">
+					<h3>Our Company</h3>
+						<div class="f_menu">
+							   <ul>
+						            <li><a href="#">About your Company</a></li>
+						     		<li><a href="#">Terms &amp; conditions</a></li>
+						     		<li><a href="#">News</a></li>
+						     		<li><a href="#">Team of professionals</a></li>	
+						     		<li><a href="#">Testimonials</a></li>					     		
+						   	   </ul>
+						</div>
+				   </div>				
+		  <div class="footer_grid4">
+			<h3>Follow US</h3>
+				<div class="img_list">
+				    <ul>
+					     <li><img src="images/28.png" alt=""><a href="#">Join Us on Facebook</a></li>
+					     <li><img src="images/twitter.png" alt=""><a href="#">Follow Us on Twitter</a></li>
+					     <li><img src="images/39.png" alt=""><a href="#">Share Us on Twitter</a></li>
+				    </ul>
+				</div>
+		 </div>
+	   <div class="clear"></div>
+     </div>
+    </div>
+  </div>
+<div class="copy_right">
+				<p>Design by : SHRUTI PURKAIT</a></p>
+		 </div>
+</body>
+</html>
